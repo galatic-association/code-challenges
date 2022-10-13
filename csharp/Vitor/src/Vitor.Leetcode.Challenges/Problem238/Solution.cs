43887a8ff1d2da3f26d6d4190688b1650f0b6a96
Vitor Muniz <vitofmuniz@gmail.com>
2022-10-13 20:13:17 -0300

    Add solution for Problem238 LeetCode

---
+++ csharp/Vitor/src/Vitor.Leetcode.Challenges/Problem238/Solution.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Security;
+
+namespace Vitor.Leetcode.Challenges.Problem238
+{
+    public class Solution
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            if (nums.Length is 0)
+                return Array.Empty<int>();
+
+            var posfix = new int[nums.Length];
+            var prefix = new int[nums.Length];
+
+            prefix[0] = nums[0];
+            posfix[nums.Length - 1] = nums[nums.Length - 1];
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                prefix[i] = prefix[i - 1] * nums[i];
+            }
+
+            for (var i = nums.Length - 1; i > 0; i--)
+            {
+                posfix[i - 1] = posfix[i] * nums[i - 1];
+            }
+
+            var answer = new int[nums.Length];
+
+            answer[0] = posfix[1];
+
+            for (var i = 1; i < nums.Length - 1; i++)
+            {
+                answer[i] = posfix[i + 1] * prefix[i - 1];
+            }
+
+            answer[nums.Length - 1] = prefix[nums.Length - 2];
+
+            return answer;
+        }
+    }
+}
---
+++ csharp/Vitor/tests/Vitor.Leetcode.Challenges.Tests/Problem238/SolutionTests.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
+using Vitor.Leetcode.Challenges.Problem238;
+using Xunit;
+
+namespace Vitor.Leetcode.Challenges.Tests.Problem238
+{
+    public class SolutionTests
+    {
+        public static IEnumerable<object[]> Scenarios
+            => new object[][]
+        {
+            new object[] { new[] { 1, 2, 3, 4 }, new[] {24, 12, 8, 6 } },
+            new object[] { new[] { -1, 1, 0, -3, 3 }, new[] { 0, 0, 9, 0, 0 } }
+        };
+
+
+        [Theory]
+        [MemberData(nameof(Scenarios))]
+        public void Should_Return_Array_With_Product_Except_Self(int[] arr, int[] expectedArray)
+        {
+            //Arrange
+            var sut = new Solution();
+
+            //Act
+            var result = sut.ProductExceptSelf(arr);
+
+            //Assert
+            result.Should().BeEquivalentTo(expectedArray);
+        }
+    }
+}
