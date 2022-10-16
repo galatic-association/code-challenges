5ee83bf9487bfd36ce249dad38173b2427fd8bec
vitor muniz <vitorfmuniz@gmail.com>
2022-10-16 11:28:06 -0300

    Add solution for Problem53 with Time Limit Exceeded error

---
+++ csharp/Vitor/src/Vitor.Leetcode.Challenges/Problem53/Solution.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Vitor.Leetcode.Challenges.Problem53
+{
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+
+            var maxSum = int.MinValue;
+
+            //i -> start windows position
+            for (var i = 0; i < nums.Length; i++)
+            {
+                //j -> windowSize
+                for (var j = 0; j < nums.Length-i; j++)
+                {
+                    var currentSum = 0;
+
+                    //k -> current index
+                    for (var k = i; k < i+j+1; k++)
+                        currentSum += nums[k];
+
+                    maxSum = Math.Max(maxSum, currentSum);
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ csharp/Vitor/tests/Vitor.Leetcode.Challenges.Tests/Problem53/SolutionTests.cs
@@ -0,0 +1,33 @@
+using FluentAssertions;
+using System.Collections.Generic;
+using Vitor.Leetcode.Challenges.Problem53;
+using Xunit;
+
+namespace Vitor.Leetcode.Challenges.Tests.Problem53
+{
+    public class SolutionTests
+    {
+        public static IEnumerable<object[]> Scenarios()
+        {
+            yield return new object[] { new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6 };
+            yield return new object[] { new int[] { 1 }, 1 };
+            yield return new object[] { new int[] { 5, 4, -1, 7, 8 }, 23 };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Scenarios))]
+        public void MaxSubArray_NaiveApproach_Should_Return_Correct_Values(int[] @case, int expectedSum)
+        {
+            //Arrange
+            var sut = new Solution();
+
+            //Act
+            var result = sut.MaxSubArray(@case);
+
+            //Assert
+            result.Should().Be(expectedSum);
+        }
+
+    }
+}
