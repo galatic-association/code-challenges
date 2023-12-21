d4bb5e546587ed3b8574b02cc2043719166f24bf
Vitor Muniz <vitofmuniz@gmail.com>
2023-12-21 16:20:34 -0300

    Add implementation problem 3 leetcode

---
+++ csharp/Vitor/src/Vitor.Leetcode.Challenges/Problem3/Solution.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vitor.Leetcode.Challenges.Problem3
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s is "")
+                return 0;
+
+            if (s is { Length: 1 })
+                return 1;
+
+            var maxSubstringLength = 0;
+
+            var set = new HashSet<char>();
+            for (var i = 0; i < s.Length - 1; i++)
+            {
+                set.Add(s[i]);
+                for (var j = i+1; j < s.Length; j++)
+                {
+                    if (set.Contains(s[j]))
+                    {
+                        break;
+                    }
+                    set.Add(s[j]);
+                }
+                maxSubstringLength = Math.Max(set.Count, maxSubstringLength);
+                set.Clear();
+            }
+
+            return maxSubstringLength;
+        }
+    }
+}
