9ca3d420cf74865889761f38de67a29b814a051f
Vitor Muniz <vitofmuniz@gmail.com>
2023-12-23 11:09:17 -0300

    Simplify leetcode problem 1768 solution

--- csharp/Vitor/src/Vitor.Leetcode.Challenges/Problem1768/Solution.cs
+++ csharp/Vitor/src/Vitor.Leetcode.Challenges/Problem1768/Solution.cs
@@ -6,36 +6,22 @@ namespace Vitor.Leetcode.Challenges.Problem1768
     {
         public string MergeAlternately(string word1, string word2)
         {
-            var pointer = 0;
-            var totalLength = word1.Length + word2.Length;
+            var pointer1 = 0;
+            var pointer2 = 0;
             var builder = new StringBuilder();
-            var current = 0;
-            while (pointer < word1.Length && pointer < word2.Length)
+ 
+            while (pointer1 < word1.Length && pointer2 < word2.Length)
             {
-                builder.Insert(current, word1[pointer]);
-                builder.Insert(current+1, word2[pointer]);
-                current += 2;
-                pointer++;
+                builder.Append(word1[pointer1++]);
+                builder.Append(word2[pointer2++]);
             }
 
-            if (pointer >= word1.Length)
-            {
-                FillRemaningValues(word2);
-            }
-            else if (pointer >= word2.Length)
-            {
-                FillRemaningValues(word1);
-            }
+            while (pointer1 < word1.Length)
+                builder.Append(word1[pointer1++]);
+            while (pointer2 < word2.Length)
+                builder.Append(word2[pointer2++]);
 
             return builder.ToString();
-
-            void FillRemaningValues(string word)
-            {
-                for (var i = current; i < totalLength; i++, pointer++)
-                {
-                    builder.Insert(i, word[pointer]);
-                }
-            }
         }
     }
 }
